df9f097872d2e40d3ad342badf3016d294c76a00
Adrian B <24711048+ButterHub@users.noreply.github.com>
2019-04-13 18:20:13 +0100

    Got einstein working again through visual studio code.

--- app/index.js
+++ app/index.js
@@ -1,1 +1,28 @@
-console.log('Hello world!');
+import clock from "clock";
+import document from "document";
+import { preferences } from "user-settings";
+
+// Update the clock every minute
+clock.granularity = "minutes";
+
+const clockElement = document.getElementById("clock");
+
+clock.ontick = e => {
+  let today = e.date;
+  let hours = today.getHours();
+
+  if (preferences.clockDisplay === "12h") {
+    hours = hours % 12 || 12;
+  }
+
+  const hours = zeroPad(hours);
+  const minutes = zeroPad(today.getMinutes());
+  clockElement.text = `${hours}:${minutes}`;
+};
+
+function zeroPad(time) {
+  if (time < 10) {
+    time = "0" + time;
+  }
+  return time;
+}
